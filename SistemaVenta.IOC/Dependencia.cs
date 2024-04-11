fbcc2f7a6acde4adea25e4aa16cfd35b9e78574f
Daniel Valenzuela <hetzondaniel1@gmial.com>
2024-04-11 10:16:00 -0600

    Commit principal

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interaces;
+//using SistemaVenta.DAL.Implemantacion;
+//using SistemaVenta.BLL.Interaces;
+//using SistemaVenta.BLL.Implemantacion;
+
+
+namespace SistemaVenta.IOC
+
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services,IConfiguration configuration) {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        
+        }
+    }
+}
