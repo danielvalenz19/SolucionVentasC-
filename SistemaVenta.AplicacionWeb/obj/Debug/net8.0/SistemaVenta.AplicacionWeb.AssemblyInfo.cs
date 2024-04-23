fd7ef8ecb4122c590308f652fc4673b6ca577d66
Daniel Valenzuela <hetzondaniel1@gmial.com>
2024-04-23 11:27:52 -0600

    Se terminaron las vistas del primer modulo planeado

--- SistemaVenta.AplicacionWeb/obj/Debug/net8.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
+++ SistemaVenta.AplicacionWeb/obj/Debug/net8.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcc2f7a6acde4adea25e4aa16cfd35b9e78574f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
